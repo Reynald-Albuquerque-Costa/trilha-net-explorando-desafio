87a0b507609ed51be9be2d07e8c493c4c055780e
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-11-10 00:44:10 -0300

    @Reynald-Albuquerque-Costa[Refatorado]

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,7 +16,6 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
             if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
@@ -24,7 +23,6 @@ namespace DesafioProjetoHospedagem.Models
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
                  throw new ArgumentOutOfRangeException("A quantidade de hospedes não pode exceder a capacidade da suíte.");
             }
         }
@@ -37,21 +35,17 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
             return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria(Suite suite)
         {
             // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
             valor = suite.ValorDiaria * DiasReservados;
             
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 valor = (suite.ValorDiaria * DiasReservados) * 90/100;
